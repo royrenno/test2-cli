485a4433425250af5c2d77e33b72853bc67f1943
roy renno <royrenno@yahoo.com>
2019-08-29 23:23:54 +0000

    first commit

---
+++ lib/test2/cli.rb
@@ -0,0 +1,8 @@
+require "test2/cli/version"
+
+module Test2
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/test2/cli/version.rb
@@ -0,0 +1,5 @@
+module Test2
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
